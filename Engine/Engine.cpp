fc681afbcb90c6b445b275bb7dfd5e7e35c728cd
gotagetfood <96447537+gotagetfood@users.noreply.github.com>
2022-10-04 15:50:39 -0600

    Add project files.

---
+++ Engine/Engine.cpp
@@ -0,0 +1,28 @@
+#include "Engine.h"
+
+namespace neu
+{
+	Renderer g_renderer;
+	InputSystem g_inputSystem;
+	AudioSystem g_audioSystem;
+	Time g_time;
+	ResourceManager g_resources;
+	PhysicsSystem g_physicsSystem;
+	EventManager g_eventManager;
+
+	void Engine::Register()
+	{
+		REGISTER_CLASS(Actor);
+		REGISTER_CLASS(AudioComponent);
+		REGISTER_CLASS(ModelComponent);
+		REGISTER_CLASS(CollisionComponent);
+		REGISTER_CLASS(PhysicsComponent);
+		REGISTER_CLASS(RBPhysicsComponent);
+		REGISTER_CLASS(PlayerComponent);
+		REGISTER_CLASS(SpriteComponent);
+		REGISTER_CLASS(SpriteAnimComponent);
+		REGISTER_CLASS(TextComponent);
+		REGISTER_CLASS(TilemapComponent);
+		REGISTER_CLASS(CameraComponent);
+	}
+}
