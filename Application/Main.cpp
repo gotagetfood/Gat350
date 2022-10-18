c8b6508232aafa4b29b881e958a631f04d7699f6
gotagetfood <96447537+gotagetfood@users.noreply.github.com>
2022-10-18 15:34:11 -0600

    3d

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -78,30 +78,25 @@ int main(int argc, char** argv)
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, NULL);
 
 	// create shader
-	std::shared_ptr<neu::Shader> vs = neu::g_resources.Get<neu::Shader>("Shaders/basic.vert",GL_VERTEX_SHADER);
-	std::shared_ptr<neu::Shader> fs = neu::g_resources.Get<neu::Shader>("Shaders/basic.frag",GL_FRAGMENT_SHADER);
-	
-	//GLuint vs = glCreateShader(GL_VERTEX_SHADER);
-	//glShaderSource(vs, 1, &vertex_shader, NULL);
-	//glCompileShader(vs);
-	//GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
-	//glShaderSource(fs, 1, &fragment_shader, NULL);
-	//glCompileShader(fs);
+	//std::shared_ptr<neu::Shader> vs = neu::g_resources.Get<neu::Shader>("Shaders/basic.vert",GL_VERTEX_SHADER);
+	//std::shared_ptr<neu::Shader> fs = neu::g_resources.Get<neu::Shader>("Shaders/basic.frag",GL_FRAGMENT_SHADER);
 
 	// create program
-	GLuint program = glCreateProgram();
-	glAttachShader(program, fs->m_shader);
-	glAttachShader(program, vs->m_shader);
-	glLinkProgram(program);
-	glUseProgram(program);
+	std::shared_ptr<neu::Program> program = neu::g_resources.Get<neu::Program>("Shaders/basic.prog");
+	program->Link();
+	program->Use();
 
-	GLint uniform1 = glGetUniformLocation(program, "scale");
-	GLint uniform2 = glGetUniformLocation(program, "tint");
-	GLint uniform3 = glGetUniformLocation(program, "transform");
+	// create texture 
+	//std::shared_ptr<neu::Texture> texture1 = neu::g_resources.Get<neu::Texture>("textrues/llama.jpg"); 
+	//std::shared_ptr<neu::Texture> texture2 = neu::g_resources.Get<neu::Texture>("textures/wood.png"); 
+	//texture1->Bind();
 
-	glm::mat4 mx{ 1 };
-	//mx = glm::scale(glm::vec3{ 0.5,0.5,0.5 });
+	// create material 
+	std::shared_ptr<neu::Material> material = neu::g_resources.Get<neu::Material>("materials/box.mtrl");
+	material->Bind();
 	
+	//fun stuff
+	glm::mat4 mx{ 1 };
 
 	bool quit = false;
 	while (!quit)
@@ -110,11 +105,12 @@ int main(int argc, char** argv)
 
 		if (neu::g_inputSystem.GetKeyState(neu::key_escape) == neu::InputSystem::KeyState::Pressed) quit = true;
 
-		glUniform1f(uniform1, std::sin(neu::g_time.time * 0.2));
-		glUniform3f(uniform2, std::sin(neu::g_time.time), std::cos(neu::g_time.time), std::tan(neu::g_time.time));
-
-		mx = glm::eulerAngleXYZ(0.0f, 0.0f, neu::g_time.time);
-		glUniformMatrix4fv(uniform3, 1, GL_FALSE, glm::value_ptr(mx));
+		//fun stuff
+		material->GetProgram()->SetUniform("tint", glm::vec3{ 1, 2, 1 });
+		material->GetProgram()->SetUniform("tint", glm::vec3{ 1, 0, 0 });
+		material->GetProgram()->SetUniform("scale", 0.5f);
+		program->SetUniform("scale", std::sin(neu::g_time.time * 3));
+		program->SetUniform("transform", mx);
 
 		neu::g_renderer.BeginFrame();
 		
--- Engine/Renderer/Renderer.cpp
+++ Engine/Renderer/Renderer.cpp
@@ -103,7 +103,7 @@ namespace neu
 
 		SDL_Point center{ (int)origin.x, (int)origin.y };
 				
-		SDL_RenderCopyEx(m_renderer, texture->m_texture, nullptr, &dest, angle, &center, SDL_FLIP_NONE);
+		//SDL_RenderCopyEx(m_renderer, texture->m_texture, nullptr, &dest, angle, &center, SDL_FLIP_NONE);
 	}
 
 	void Renderer::Draw(std::shared_ptr<Texture> texture, const Transform& transform, const Vector2& registration)
@@ -122,7 +122,7 @@ namespace neu
 
 		SDL_Point center{ (int)origin.x, (int)origin.y };
 
-		SDL_RenderCopyEx(m_renderer, texture->m_texture, nullptr, &dest, transform.rotation, &center, SDL_FLIP_NONE);
+		//SDL_RenderCopyEx(m_renderer, texture->m_texture, nullptr, &dest, transform.rotation, &center, SDL_FLIP_NONE);
 	}
 
 	void Renderer::Draw(std::shared_ptr<Texture> texture, const Rect& source, const Transform& transform, const Vector2& registration, bool flipH)
@@ -150,7 +150,7 @@ namespace neu
 		SDL_Point center{ (int)origin.x, (int)origin.y };
 
 		SDL_RendererFlip flip = (flipH) ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE;
-		SDL_RenderCopyEx(m_renderer, texture->m_texture, &src, &dest, math::RadToDeg(mx.GetRotation()), &center, flip);
+		//SDL_RenderCopyEx(m_renderer, texture->m_texture, &src, &dest, math::RadToDeg(mx.GetRotation()), &center, flip);
 	}
 
 }
